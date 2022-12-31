7a4ddbb426b24f6a2d9cf9024f4e9f532d10afde
kaushikbhaisavani <kaushik.savani1712@gmail.com>
2022-12-31 16:32:11 +0530

    class added

--- lib/banneradd.dart
+++ lib/banneradd.dart
@@ -55,3 +55,73 @@ class _BannerPageState extends State<BannerPage> {
       ..load();
   }
 }
+
+//class for anywhere use
+/*
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class BannerAdmob extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _BannerAdmobState();
+  }
+}
+
+class _BannerAdmobState extends State<BannerAdmob> {
+  BannerAd _bannerAd;
+  bool _bannerAdIsLoaded = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _bannerAd = BannerAd(
+        size: AdSize.largeBanner,
+        adUnitId: Platform.isAndroid
+            ? 'ca-app-pub-3940256099942544/6300978111'
+            : 'ca-app-pub-3940256099942544/2934735716',
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) {
+            print('$BannerAd loaded.');
+            setState(() {
+              _bannerAdIsLoaded = true;
+            });
+          },
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            print('$BannerAd failedToLoad: $error');
+            setState(() {
+              _bannerAdIsLoaded = true;
+            });
+            ad.dispose();
+          },
+          onAdOpened: (Ad ad) => print('$BannerAd onAdOpened.'),
+          onAdClosed: (Ad ad) => print('$BannerAd onAdClosed.'),
+        ),
+        request: AdRequest())
+      ..load();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bannerAd.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return (_bannerAdIsLoaded && _bannerAd != null)
+        ? Container(
+        height: _bannerAd.size.height.toDouble(),
+        width: _bannerAd.size.width.toDouble(),
+        child: AdWidget(ad: _bannerAd))
+        : Container();
+  }
+}
+
+*/
